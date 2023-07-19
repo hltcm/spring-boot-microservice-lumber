61ceac4e04cd3d57dc56d2e39d08d61c6fb519f3
Halit Cem <halitcem@gmail.com>
2023-07-20 02:05:03 +0300

    Logback, Logstash, Elasticsearch and Kibana configured.

--- lumber-service/src/main/java/com/tomruk/lumberservice/LumberServiceApplication.java
+++ lumber-service/src/main/java/com/tomruk/lumberservice/LumberServiceApplication.java
@@ -1,15 +1,18 @@
 package com.tomruk.lumberservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@Slf4j
 public class LumberServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LumberServiceApplication.class, args);
+		log.error("Halit");
 	}
 
 }
